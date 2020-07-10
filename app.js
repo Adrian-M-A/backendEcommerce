dbfa6437ac9ec75cdb0302f907d57263ba7483e9
Adrian M.A <adrian@neurocadi.es>
2020-07-10 19:52:00 +0200

    User model ready

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require ("express");
 const app = express();
 const PORT = 3000;
+require("./config/mongoose");
+
+        //We import the model
+const User = require("./models/User");
+
 
 
 
---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require("validator");
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },  
+    surnames: {
+        type: String,
+        required: true
+    },  
+    adress: {
+        type: String,
+        required: true
+    },  
+    phone: String,
+    document: {
+        type: String,
+        required: true,
+    },  
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Email required."],
+        validate: {
+            validator: function(email) {
+                return validator.isEmail(email)
+            },
+            message: props => `${props.value} is not a valid Email!`
+        },
+
+    },  
+    password: {
+        type: String,
+        required: [true, "Password required."],
+        minlength: 8
+    },  
+    token: String
+}, {
+    timestamps:true
+});
+
+UserSchema.methods.toJSON = function() {
+    const user = this.toObject();
+    delete user.password;
+    delete user.tokens;
+    return user;
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
